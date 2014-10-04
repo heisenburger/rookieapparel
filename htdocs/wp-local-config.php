be782f1cd1cdabb6790a719ec7f3522cccab610b
heisenburger <serenedesigner@gmail.com>
2014-10-05 12:39:46 +1300

    clean install

---
+++ htdocs/wp-local-config.php
@@ -0,0 +1,15 @@
+<?php 
+// Local server settings
+ 
+// Local Database
+define('DB_NAME', 'rook_rapparel');
+define('DB_USER', 'tomatoweeni');
+define('DB_PASSWORD', 'limefishSUSHIBRAIN');
+define('DB_HOST', 'localhost');
+ 
+// Overwrites the database to save keep edeting the DB
+define('WP_HOME','http://localhost/rookieapparel/htdocs');
+define('WP_SITEURL','http://localhost/rookieapparel/htdocs');
+ 
+// Turn on debug for local environment
+define('WP_DEBUG', true);
